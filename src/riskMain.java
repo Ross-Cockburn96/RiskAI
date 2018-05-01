e030864a2ce957f612ec83e1615e66528490c5d0
Ross <ross.s.cockburn@gmail.com>
2018-05-01 21:14:27 +0100

    added comments

--- src/riskMain.java
+++ src/riskMain.java
@@ -18,18 +18,18 @@ public class riskMain {
             System.out.println(c.getRegion());
         }
     }
-    private static void deckCreator()throws IOException{
+    private static void deckCreator()throws IOException{    //reads riskCards.txt file and creates risk playing card objects
         deck = new ArrayList<>();
         File f = new File("res/riskCards.txt");
         BufferedReader b = new BufferedReader(new FileReader(f));
         String readline = "";
-        while ((readline = b.readLine()) != null) {
-            String region = readline.split("-")[0];
+        while ((readline = b.readLine()) != null) {     //loops through each line in the text file until there is no more content
+            String region = readline.split("-")[0]; //each line of the file consits of {region} - {unit type} so we split on a regex of '-'
             System.out.println(region);
             String unit = readline.split("-")[1];
-            deck.add(new Card(region, unit));
+            deck.add(new Card(region, unit));   //creates new card data type and adds it to deck
         }
-        Collections.shuffle(deck);
+        Collections.shuffle(deck);  //shuffles deck randomly
     }
 
 }
