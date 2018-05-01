24c22efdae6609e5e8cd930f361d306128c79341
Ross <ross.s.cockburn@gmail.com>
2018-05-01 20:53:43 +0100

    Creates deck and shuffles

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,15 @@
 public class Card {
+    private String region;
+    private String unitType;
     public Card(String region, String unitType ){
-
+        this.region = region;
+        this.unitType = unitType;
+    }
+    public String getRegion(){
+        return region;
+    }
+    public String getUnitType(){
+        return unitType;
     }
 }
 
--- src/riskMain.java
+++ src/riskMain.java
@@ -2,22 +2,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class riskMain {
-
+    private static List<Card> deck ;
     public static void main(String[] args) throws IOException{
         try {
             deckCreator();
         }catch(IOException e){
             e.printStackTrace();
         }
+        for(Card c : deck){
+            System.out.println(c.getRegion());
+        }
     }
     private static void deckCreator()throws IOException{
+        deck = new ArrayList<>();
         File f = new File("res/riskCards.txt");
         BufferedReader b = new BufferedReader(new FileReader(f));
         String readline = "";
         while ((readline = b.readLine()) != null) {
-            System.out.println(readline);
+            String region = readline.split("-")[0];
+            System.out.println(region);
+            String unit = readline.split("-")[1];
+            deck.add(new Card(region, unit));
         }
+        Collections.shuffle(deck);
     }
+    
 }
