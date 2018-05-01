b07eb0a2257dde7e25069708b8263aa12b05ebb8
Ross <ross.s.cockburn@gmail.com>
2018-05-01 20:36:42 +0100

    Reads text file containing card details

--- src/riskMain.java
+++ src/riskMain.java
@@ -1,6 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class riskMain {
 
-    public static void main(String[] args){
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException{
+        try {
+            deckCreator();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    private static void deckCreator()throws IOException{
+        File f = new File("res/riskCards.txt");
+        BufferedReader b = new BufferedReader(new FileReader(f));
+        String readline = "";
+        while ((readline = b.readLine()) != null) {
+            System.out.println(readline);
+        }
     }
 }
